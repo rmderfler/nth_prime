ad5aa29bf0843b4faa7832f2bcc2788c95f1aa83
Derfler <rmderfler@gmail.com>
2014-07-24 21:26:14 -0700

    it works

--- lib/nth_prime.rb
+++ lib/nth_prime.rb
@@ -1,3 +1,21 @@
 def nth_prime (number)
-	3
+  number = number.to_i
+  next_number_check = 3 
+  divisor = next_number_check-1
+  primes_array = [2]
+  while primes_array.length < number
+   modulo = next_number_check % divisor
+    while modulo!= 0
+      divisor -= 1
+      modulo = next_number_check % divisor
+      if divisor == 1
+        primes_array << next_number_check
+      end
+      end
+    next_number_check += 1
+    divisor=next_number_check - 1
 end
+  primes_array.pop()
+end
+
+
--- spec/nth_prime_spec.rb
+++ spec/nth_prime_spec.rb
@@ -6,5 +6,9 @@ describe ("nth prime") do
 	it('returns the nth prime') do
 		nth_prime(2).should(eq(3))
 	end
+
+	it('works on higher numbers') do
+		nth_prime(306).should(eq(2017))
+	end
 	
 end
